9a5b81a16a7cff189670c6d0a79935451688ea42
Jonas <113011860+Goblinmine@users.noreply.github.com>
2024-06-11 19:16:28 +0200

    added template project

---
+++ src/client/java/net/goblinmine/minecart_tweaks/GoblinminesMinecartTweaksClient.java
@@ -0,0 +1,10 @@
+package net.goblinmine.minecart_tweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoblinminesMinecartTweaksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/goblinmine/minecart_tweaks/GoblinminesMinecartTweaksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.goblinmine.minecart_tweaks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GoblinminesMinecartTweaksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
