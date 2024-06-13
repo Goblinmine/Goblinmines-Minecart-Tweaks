0476d9e8aa2da81466e4826ec0d60c936db7c8a7
Jonas <113011860+Goblinmine@users.noreply.github.com>
2024-06-13 11:32:57 +0200

    inital commit for Architectury test

---
+++ common/src/main/java/net/goblinmine/gmt/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.goblinmine.gmt;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "goblinmines_minecart_tweaks";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric-like/src/main/java/net/goblinmine/gmt/fabriclike/ExampleModFabricLike.java
@@ -0,0 +1,10 @@
+package net.goblinmine.gmt.fabriclike;
+
+import net.goblinmine.gmt.ExampleMod;
+
+public final class ExampleModFabricLike {
+    public static void init() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
