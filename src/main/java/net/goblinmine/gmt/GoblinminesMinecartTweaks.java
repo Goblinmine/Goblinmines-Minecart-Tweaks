ee86a7d7aeda9aa0726475f2fce265040ab005ce
Jonas <113011860+Goblinmine@users.noreply.github.com>
2024-06-17 19:11:20 +0200

    added 2 new gamerules to set the speed of the powered and copper rails

--- src/main/java/net/goblinmine/gmt/GoblinminesMinecartTweaks.java
+++ src/main/java/net/goblinmine/gmt/GoblinminesMinecartTweaks.java
@@ -2,12 +2,15 @@ package net.goblinmine.gmt;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +20,13 @@ public class GoblinminesMinecartTweaks implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("Goblinmine's Minecart Tweaks");
     public static final String MOD_ID = "goblinmines_minecart_tweaks";
+
+    public static final GameRules.Key<GameRules.IntRule> POWERED_RAIL_SPEED =
+            GameRuleRegistry.register("poweredRailSpeed", GameRules.Category.MISC,
+                    GameRuleFactory.createIntRule(24, 0, 100));
+    public static final GameRules.Key<GameRules.IntRule> COPPER_RAIL_SPEED =
+            GameRuleRegistry.register("copperRailSpeed", GameRules.Category.MISC,
+                    GameRuleFactory.createIntRule(10, 0, 100));
 
     @Override
     public void onInitialize() {
